cebf9d69fcf2cd8b88581ea8d3c638f4781dde6f
Jose Sanchez <sanchezmartinezjosecarlos@gmail.com>
2020-03-12 17:12:48 +0000

    Morgan logger & error handling

--- src/app.js
+++ src/app.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const bodyParser = require('body-parser')
 
 const productRoutes = require('../api/routes/menu');
 const orderRoutes = require('../api/routes/order')
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use('/products', productRoutes);
 app.use('/order', orderRoutes);
 
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+})
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+})
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`app listening on port ${port}`))
