47eefd586f672f87c548ea9869b8f1b669011f73
Jose Sanchez <sanchezmartinezjosecarlos@gmail.com>
2020-04-29 18:48:42 +0100

    Order.js code refactorized, promises to async/await

--- controllers/order.js
+++ controllers/order.js
@@ -1,70 +1,59 @@
 
 const Order = require('../models/orders');
 
-
-function findAllOrders(req, res, next) {
-  return Order.find({})
-    .populate('product')
-    .then(orders => {
-      return orders.length <= 0 ?
-        res.status(404).json({ message: "No orders yet" }) :
-        res.status(200).json({
-          count: orders.length,
-          orders
-        })
-    })
-    .catch(err => console.log(err))
+async function findAllOrders(req, res, next) {
+  try {
+    const orders = await Order.find({}).populate('product');
+    if (!orders) return res.status(404).json({ message: "No orders yet" })
+    else return res.status(200).json({ count: orders.length, orders })
+  }
+  catch (e) {
+    next(e)
+  }
 }
 
-function addOrder(req, res, next) {
+async function addOrder(req, res, next) {
   const order = {
     product: req.body.product,
     quantity: req.body.quantity
   }
-  return Order.create(order).then(customerOrder => {
-    res.status(201).json({ customerOrder })
-  })
-    .catch(err => console.log(err))
+  try {
+    const userOrder = await Order.create(order);
+    if (!userOrder) return res.status(404).json({ message: "No product ordered yet" })
+    else return res.status(201).json({ userOrder })
+  }
+  catch (e) {
+    next(e)
+  }
 }
 
-function getOrderDetails(req, res, next) {
-  return Order.findById(req.params.orderId)
-    .populate('product')
-    .then(uniqueOrder => {
-      res.json({ uniqueOrder })
-    })
-    .catch(function (err) {
-      res.json(err);
-    });
+async function getOrderDetails(req, res, next) {
+  try {
+    const uniqueOrder = await Order.findById(req.params.orderId).populate('product');
+    if (!uniqueOrder) return res.status(404).json({ message: "Order not found with such ID" })
+    else return res.status(200).json({ uniqueOrder })
+  }
+  catch (e) { next(e) }
 }
 
-function updateOrder(req, res, next) {
-  return Order.findByIdAndUpdate({ _id: req.params.orderId }, { $set: req.body }, { new: true }).then(order => {
-    res.status(200).json({
-      message: "order updated",
-      order
-    })
-  })
+async function updateOrder(req, res, next) {
+  try {
+    const order = await Order.findByIdAndUpdate({ _id: req.params.orderId }, { $set: req.body }, { new: true });
+    if (!order) return res.status(404).json({ message: "Unable to update order with such ID" })
+    else return res.status(200).json({ message: "order updated", order })
+  }
+  catch (e) { next(e) }
 }
 
-function deleteOrder(req, res, next) {
-  return Order.findByIdAndRemove({ _id: req.params.orderId }).then(deletedOrder => {
-    return (!deletedOrder) ?
-      res.status(500).json("Cant delete unexistent ID´s") :
-      res.status(200).json({
-        message: "deleted order",
-        deletedOrder
-      })
-  })
-    .catch(err => {
-      res.status(500).json({
-        message: "Incorrect ID character length",
-        error: err
-      })
-    })
+async function deleteOrder(req, res, next) {
+  try {
+    const orderID = await Order.findByIdAndRemove({ _id: req.params.orderId });
+    if (!orderID) return res.status(404).json({ message: "Can´t delete order with such ID" })
+    else return res.status(200).json({ message: "Order deleted", orderID })
+  } catch (e) {
+    next(e)
+  }
 }
-
-
 
 module.exports = {
   findAllOrders,
